3274df0d40a1c7cd4fabf3f9df855c74ad8bb8b5
yousefhoseini76 <56256752+yousefhoseini76@users.noreply.github.com>
2020-05-21 16:45:05 +0430

    Add initial codes

--- s1.py
+++ s1.py
@@ -1,1 +1,32 @@
-print("Hello github world")
+import random
+
+colors = {
+    1: "red",
+    2: "blue",
+    3: "green",
+    4: "yellow",
+    5: "white"
+}
+
+initial = []
+while len(initial) < 4:
+    r = random.randint(1,5)
+    if r not in initial:
+        initial.append(r)
+
+print([colors[item] for item in initial])
+
+guess = input()
+guess = guess.split(",")
+guess = [int(item) for item in guess]
+print(guess)
+
+black = 0
+white = 0
+for i in range(len(guess)):
+    if initial[i] == guess[i]:
+        black += 1
+    elif guess[i] in initial:
+        white += 1
+
+print("black: {} , white: {}".format(black , white))
